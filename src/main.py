59a753abd64b498a193563d7d57781923a99423e
andres-av <andres.av.dev@gmail.com>
2022-11-17 19:46:20 +0000

    beginning of exercise

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, json 
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Location, Episode, Favorites
+from datetime import timedelta
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -30,14 +31,117 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
+@app.route('/character', methods=['GET'])
+def handle_character():
+    character = Character.query.all()
+    characterList = list(map(lambda obj : obj.serialize(),character))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "success": True,
+        "result": characterList
+    }
+    return jsonify(response_body), 200
+
+# USER ENDPOINTS
+@app.route('/user', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = list(map(lambda obj : obj.serialize(),users))
+    response_body = {
+        
+        "success": True,
+        "results": user_list
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def users_by_id(user_id):
+    UserByid = User.query.get(user_id)
+    print(UserByid)
+    user_by_id = UserByid.serialize()
+    response_body = {
+        "success": True,
+        "result": user_by_id
+    }
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    body = json.loads(request.data)
+    new_user = User(password = body ["password"], email = body["email"])
+    db.session.add(new_user)
+    db.session.commit()
+    response_body = {
+        "msg": ("user created",new_user.serialize())
+    }
+
+    return jsonify(response_body), 200
+
+# END of USER ENDPOINTS
+# FAVORITE ENDPOINTS
+@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    favorites = Favorites.query.filter_by(user_id =user_id)
+    favoritesList = list(map(lambda obj : obj.serialize(),favorites))
+    response_body = {
+        "msg": ("List of favorites:",favoritesList) 
+    }
+    return jsonify(response_body),200
+
+@app.route('/user/<int:user_id>', methods=['POST'])   
+def post_new_favorite(user_id):
+    body=json.loads(request.data)
+    new_favorite = Favorites(name=body["name"], user_id =body ["user_id"])   
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    response_body = {
+        "success": True,
+        "result": 'Favorite created'
+    }
+    return jsonify(response_body,200)
+
+@app.route('/user/<int:user_id>/favorites/<int:favorites_id>', methods=['DELETE'])
+def delete_favorites(user_id,favorites_id):
+    favorite_to_delete = Favorites.query.filter_by(id = favorites_id).all()
+    db.session.delete(favorite[0])
+    db.session.commit()
+    response_body = {
+        "msg": "Your favorite item has been deleted!"
+    }
+    return jsonify(response_body),200
+
+# ---  ROUTE CHARACTERS ---
+@app.route('/character', methods=['GET'])
+def get_character_list():
+    characters = Character.query.all()
+    character_list = list(map(lambda obj : obj.serialize(),characters))
+    response_body = {
+        "msg": character_list
+    }
+    return jsonify(response_body), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def show_character(character_id):
+    characterId = Character.query.get(character_id)
+    print(characterId)
+    return jsonify(characterId.serialize()), 200
+
+# ---  ROUTE PLANETS ---
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planets = Planet.query.all()
+    planet_list = list(map(lambda obj : obj.serialize(),planets))
+    response_body = {
+        "msg": planet_list
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def show_planet(planet_id):
+    planetId = Planet.query.get(planet_id)
+    print(planetId)
+    return jsonify(planetId.serialize()), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,8 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +16,101 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    species = db.Column(db.String(80), unique=False, nullable=False)
+    type_character = db.Column(db.String(80), unique=False, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    origin = db.Column(db.String(80), unique=False, nullable=False)
+    location = db.Column(db.String(80), unique=False, nullable=False)
+    image = db.Column(db.String(80), unique=False, nullable=False)
+    episode = db.Column(db.String(80), unique=False, nullable=False)
+    url = db.Column(db.String(80), unique=False, nullable=False)
+    created = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+            "type_character": self.type_character,
+            "gender": self.gender,
+            "origin": self.origin,
+            "location": self.location,
+            "image": self.image,
+            "episode": self.episode,
+            "url": self.url,
+            "created": self.created,
+        }
+        
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    type = db.Column(db.String(250), unique=False, nullable=False)
+    dimension = db.Column(db.String(250), unique=False, nullable=False)
+    residents = db.Column(db.String(250), unique=False, nullable=False)
+    url = db.Column(db.String(250), unique=False, nullable=False)
+    created = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Location %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "dimension": self.dimension,
+            "residents": self.residents,
+            "url": self.url,
+            "created": self.created,
+            # do not serialize the password, its a security breach
+        }
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    air_date = db.Column(db.String(250), unique=False, nullable=False)
+    episode = db.Column(db.String(250), unique=False, nullable=False)
+    characters = db.Column(db.String(250), unique=False, nullable=False)
+    url = db.Column(db.String(250), unique=False, nullable=False)
+    created = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Episode %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "air_date": self.air_date,
+            "episode": self.episode,
+            "characters": self.characters,
+            "url": self.url,
+            "created": self.created,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    fk_location = db.Column(db.Integer, db.ForeignKey('location.id'))
+        
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "fk_location": self.fk_location,
+            # do not serialize the password, its a security breach
+        }                              
